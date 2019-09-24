99cff7e344a849d703bffe0142c461a94e0d7bcb
hasparus <hasparus@gmail.com>
2019-09-24 21:52:14 +0200

    Add another image as 02.png

--- .03.finished.ts
+++ .03.finished.ts
@@ -1,24 +1,22 @@
 import { Brand } from 'nom-ts';
-import { Option, some, none } from 'fp-ts/lib/Option';
+import { Either, left, right } from 'fp-ts/lib/Either';
 
 type Even = Brand<number, 'Even'>;
 const isEven = (x: unknown) => typeof x === 'number' && x % 2 === 0;
 
 const Even = (x: number): Even => {
   if (!isEven(x)) {
-    throw new Error(`${x} is not Even`);
+    throw new Error(`${x} is not even`);
   }
   return x as Even;
 };
 
-const makeEven = (x: number): Option<Even> =>
-  isEven(x) ? some(x as Even) : none;
+const makeEven = (x: number): Either<Error, Even> =>
+  isEven(x) ? right(x as Even) : left(new Error(`${x} is not even`));
 
 // ---
 
 console.log(makeEven(3), makeEven(2));
-
-console.log(Even(2));
 
 try {
   console.log(Even(3));
