ea89b8ffb0af45bdcd46a6eb20c769481196f2e6
hasparus <hasparus@gmail.com>
2020-02-05 22:33:46 +0100

    Reorder 01x*

--- 01x1.py
+++ 01x1.py
@@ -1,21 +1,12 @@
 import operator
 
 def pairAdd(xs, ys):
+  """
+  addition of vectors
+
+  pairAdd([x1, x2, x3], [y1, y2, y3]) === [x1 + y1, x2 + y2, x3 + y3]
+  """
   return map(operator.add, xs, ys)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
